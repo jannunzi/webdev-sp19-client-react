8e6812fe786a37a35f223447fda464ef933edc6d
jannunzi <jannunzi@gmail.com>
2019-01-25 18:08:17 -0500

    cleaned up white board

--- src/components/WhiteBoard.js
+++ src/components/WhiteBoard.js
@@ -1,11 +1,4 @@
 import React, {Component} from 'react'
-import CourseCard from "./CourseCards";
-import ModuleList from "./ModuleList";
-import LessonTabs from "./LessonTabs";
-import TopicPills from "./TopicPills";
-import CourseEditor from "./CourseEditor";
-import Message from "./Message";
-import Stateless from "./Stateless";
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 import CourseGrid from './CourseGrid'
 import CourseTable from './CourseTable'
