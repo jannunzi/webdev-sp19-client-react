8cb9382fe1301bbbb4074484020997625036071b
jannunzi <jannunzi@gmail.com>
2019-01-25 18:07:14 -0500

    added course table

--- src/components/WhiteBoard.js
+++ src/components/WhiteBoard.js
@@ -8,6 +8,7 @@ import Message from "./Message";
 import Stateless from "./Stateless";
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 import CourseGrid from './CourseGrid'
+import CourseTable from './CourseTable'
 class WhiteBoard extends Component {
   render() {
     return (
@@ -15,9 +16,12 @@ class WhiteBoard extends Component {
         <h1>White Board</h1>
         <Router>
           <div>
-            <Link to="/">Course Grid</Link>
-            <Route path='/'
+            <Link to="/">Course Grid</Link> |
+            <Link to="/table">Course Table</Link>
+            <Route path='/' exact
                    component={CourseGrid}/>
+            <Route path='/table'
+                   component={CourseTable}/>
           </div>
         </Router>
       </div>
