cc46006d373f55b9cb60dd08d4c6f1fd25405b61
jannunzi <jannunzi@gmail.com>
2019-01-25 17:59:18 -0500

    cleanup WhiteBoard

--- src/components/WhiteBoard.js
+++ src/components/WhiteBoard.js
@@ -13,36 +13,13 @@ class WhiteBoard extends Component {
     return (
       <div>
         <h1>White Board</h1>
+        <Link to="/grid">Course Grid</Link>
         <Router>
           <div>
-            <Link to="/modules">Modules</Link>
-            <Link to="/topics">Topics</Link>
-            <Link to="/lessons">Lessons</Link>
-            <Link to="/grid">Course Grid</Link>
             <Route path='/grid'
                    component={CourseGrid}/>
-            <Route path='/modules'
-                   component={ModuleList}/>
-            <Route path='/topics'
-                   component={TopicPills}/>
-            <Route path='/lessons'
-                   component={LessonTabs}/>
           </div>
         </Router>
-        <ModuleList/>
-        <Stateless message="Stateless World"/>
-        <Message message="life is good!"/>
-        <CourseEditor/>
-        <TopicPills/>
-        <LessonTabs/>
-        <div className="card-deck">
-          <CourseCard/>
-          <CourseCard/>
-          <CourseCard/>
-          <CourseCard/>
-          <CourseCard/>
-        </div>
-        <CourseGrid/>
       </div>
     )
   }
