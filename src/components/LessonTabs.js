2e0b83303fec4e070f6f3af1189448a556c9d38e
jannunzi <jannunzi@gmail.com>
2019-01-26 08:13:00 -0500

    refactored lesson tabs to stateless

--- src/components/LessonTabs.js
+++ src/components/LessonTabs.js
@@ -1,18 +1,15 @@
 import React from 'react'
 
-export default class LessonTabs
-  extends React.Component {
-  render() { return(
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a className="nav-link active"
-           href="#">Active Tab
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link"
-           href="#">Another Tab
-        </a>
-      </li>
-    </ul>
-  );}}
+const LessonTabs = () =>
+  <ul className="nav nav-tabs">
+    <li className="nav-item">
+      <a className="nav-link active"
+         href="#">Active Tab
+      </a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link"
+         href="#">Another Tab
+      </a>
+    </li>
+  </ul>
