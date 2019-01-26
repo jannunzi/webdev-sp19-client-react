99b05070ef416029e611803f36ab027e94ca69f2
jannunzi <jannunzi@gmail.com>
2019-01-26 08:03:42 -0500

    converted module list item to stateless

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -1,15 +1,8 @@
 import React from 'react'
 
-class ModuleListItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return(
-      <li className="list-group-item">
-        {this.props.title}
-      </li>
-    )
-  }
-}
+const ModuleListItem = ({title}) =>
+    <li className="list-group-item">
+      {title}
+    </li>
+
 export default ModuleListItem;
