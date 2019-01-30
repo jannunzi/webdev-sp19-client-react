a299c7ebb01b15bae5d5cefb9175e39d4976ee6b
Jose Annunziato <jannunzi@gmail.com>
2019-01-29 23:03:14 -0500

    display widget type as a dropdown

--- src/components/WidgetComponent.js
+++ src/components/WidgetComponent.js
@@ -5,6 +5,10 @@ import ImageWidget from './ImageWidget'
 const WidgetComponent = ({widget, deleteWidget}) =>
     <div>
         <button onClick={() => deleteWidget(widget)}>Delete</button>
+        <select className="form-control" value={widget.type}>
+            <option value="HEADING">Heading</option>
+            <option value="IMAGE">Image</option>
+        </select>
         {
             widget.type=='HEADING' && <HeadingWidget widget={widget}/> ||
             widget.type=='IMAGE'   && <ImageWidget   widget={widget}/>
